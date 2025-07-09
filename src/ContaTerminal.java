0d37326c1d2291f8818b2cc38beba4b96d88d790
ostrowskii <vanessaostroski08@gmail.com>
2025-07-09 15:18:09 -0400

    up

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -18,7 +18,7 @@ public class ContaTerminal {
         System.out.println("Saldo.: ");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo " + saldo +"já está disponível para saque");
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo " + saldo +" já está disponível para saque");
 
     }
 }
